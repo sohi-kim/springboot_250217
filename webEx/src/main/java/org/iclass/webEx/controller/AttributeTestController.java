83ed9dfe1c5275f5d54011ca40a5da759d96ec6e
kimsohee-around <soh.kim.veron@gmail.com>
2025-02-19 15:45:43 +0900

    day03 애트리뷰트

--- webEx/src/main/java/org/iclass/webEx/controller/AttributeTestController.java
+++ webEx/src/main/java/org/iclass/webEx/controller/AttributeTestController.java
@@ -1,5 +1,8 @@
 package org.iclass.webEx.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.iclass.webEx.dto.TestDto;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -16,16 +19,24 @@ public class AttributeTestController {
 	//★요청 URL 에 경로(path) 를 추가 : 파일관리, URL 관리를 위함
 	@GetMapping("/community/list")
 	public String list(Model model) {
+		List<String> list = new ArrayList<>();
+		list.add("소녀시대");
+		list.add("nuz");
+		list.add("아이브");
+		list.add("twice");
 		
 		//View community/list.html 에 전달되는 데이터
 		model.addAttribute("page", 3);
 		model.addAttribute("testDto", new TestDto());
+		model.addAttribute("list", list);
 		return "community/list";  //★추가된 경로 이름으로 폴더만들어서 지정
 	}
 	
 	@GetMapping("/community/read")
-	public String read() {
+	public String read(Model model) {
+		TestDto dto = new TestDto("홍길순",27,"인천","여자");
 		
+		model.addAttribute("dto", dto);
 		return "community/read";
 	}
 
