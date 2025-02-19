d20f58f049bfe518bbf29d99c773e97ec4264290
kimsohee-around <soh.kim.veron@gmail.com>
2025-02-19 14:43:11 +0900

    애트리뷰트(Model 사용)

--- webEx/src/main/java/org/iclass/webEx/controller/AttributeTestController.java
+++ webEx/src/main/java/org/iclass/webEx/controller/AttributeTestController.java
@@ -1,18 +1,26 @@
 package org.iclass.webEx.controller;
 
+import org.iclass.webEx.dto.TestDto;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Controller
 public class AttributeTestController {
-	// 요청 URL 에 경로(path) 를 추가 : 파일관리, URL 관리를 위함
+	//화면(View) 에 전달할 데이터를 저장하는 방법
+	//   View 는 서버에서 처리하는 템플릿엔진(타임리프) html 파일이어야 함
+	
+	
+	//★요청 URL 에 경로(path) 를 추가 : 파일관리, URL 관리를 위함
 	@GetMapping("/community/list")
-	public String list() {
+	public String list(Model model) {
 		
-		return "community/list";  //추가된 경로 이름으로 폴더만들어서 지정
+		//View community/list.html 에 전달되는 데이터
+		model.addAttribute("page", 3);
+		model.addAttribute("testDto", new TestDto());
+		return "community/list";  //★추가된 경로 이름으로 폴더만들어서 지정
 	}
 	
 	@GetMapping("/community/read")
