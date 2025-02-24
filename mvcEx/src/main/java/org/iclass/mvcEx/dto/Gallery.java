0f6d0418edd02366f9159a7180e7292b9932f2ad
kimsohee-around <soh.kim.veron@gmail.com>
2025-02-24 14:58:18 +0900

    day06 파일업로드

--- mvcEx/src/main/java/org/iclass/mvcEx/dto/Gallery.java
+++ mvcEx/src/main/java/org/iclass/mvcEx/dto/Gallery.java
@@ -1,13 +1,17 @@
 package org.iclass.mvcEx.dto;
 
+import java.util.List;
+
 import org.springframework.web.multipart.MultipartFile;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
@@ -18,9 +22,19 @@ public class Gallery {
 	private String writer;
 	// 1개의 업로드 파일을 저장할 변수
 	private MultipartFile file;
+	// 여러개의 업로드 파일을 저장할 변수
+	private List<MultipartFile> files;
 
 	// gallery.html 의 input 요소 외에 db에 파일명을 저장하기 위한 변수
 	private String fileNames;
+	
+	public Gallery(String title, String writer, String fileNames) {
+		this.title=title;
+		this.writer=writer;
+		this.fileNames=fileNames;
+	}
+	
+	
 }
 
 
