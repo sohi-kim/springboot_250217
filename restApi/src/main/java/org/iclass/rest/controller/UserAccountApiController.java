ab435f64d6fb80f9f462331bb52093e26a3e6284
kimsohee-around <soh.kim.veron@gmail.com>
2025-02-27 16:51:21 +0900

    day09 rest 컨트롤러

--- restApi/src/main/java/org/iclass/rest/controller/UserAccountApiController.java
+++ restApi/src/main/java/org/iclass/rest/controller/UserAccountApiController.java
@@ -5,6 +5,8 @@ import org.iclass.rest.service.UserAccountService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -19,4 +21,23 @@ public class UserAccountApiController {
 		UserAccount account = service.userInfo(userid);
 		return ResponseEntity.ok().body(account);
 	}
+	
+	@PostMapping("/account")  // 무결성 위반이면 badRequest 응답 보내기
+	public ResponseEntity<?> post(@RequestBody UserAccount dto){
+		
+		try {
+			int result = service.regist(dto);
+			return ResponseEntity.ok().body(result);
+		}catch (Exception e) {
+			return ResponseEntity.badRequest().build();
+		}
+		
+	}
+	
+	
+	
+	
+	
+	
+	
 }
